1206cf1f50162a51e0693c201009f0ea0af55d04
shoffkarim <k4r1mch1k@gmail.com>
2021-05-19 11:26:47 +0500

    add message components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Message } from './components';
 
 const App:React.FC = () => {
   return (
     <div className="App">
-
+      <Message isMy={false}/>
+      <Message isMy={true}/>
     </div>
   );
 }
---
+++ src/components/Message/Message.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./message.sass";
+import classNames from 'classnames';
+import avatar from "../../assets/img/default-avatar.jpg";
+
+interface MessageProps {
+  isMy: boolean
+}
+
+const Message: React.FC<MessageProps> = ({isMy}) => {
+  return (
+    <div className={classNames("message", isMy ? "my" : "friend")}>
+      <div className="message-wrapper">
+        {!isMy &&
+          <div className="message__avatar">
+            <img src={avatar} alt="" />
+          </div>
+        }
+        <div className="message__bubble">
+          <p>Hello, World!</p>
+        </div>
+      </div>
+      <div className="message__time">
+        3 min ago
+      </div>
+    </div>
+  )
+}
+
+export default Message
